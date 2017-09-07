210fe4150a072cab2d73829b87a9c4238394400d
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-09-07 11:11:28 +0900

    Logic

---
+++ FrontendAssembly/Class1.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace FrontendAssembly;
+using namespace Platform;
+using namespace Windows::Storage;
+
+struct _libretro_iobuf
+{
+	StorageFile^ file;
+};
+
+EXTERN_C_START
+RFILE *frontend_filestream_open(const char *path, unsigned mode)
+{
+	return nullptr;
+}
+
+int frontend_filestream_close(RFILE *stream)
+{
+	auto path = stream->file->Path;
+	return 0;
+}
+EXTERN_C_END
+
+Class1::Class1()
+{
+	retro_set_file_open(frontend_filestream_open);
+	retro_set_file_close(frontend_filestream_close);
+}
---
+++ FrontendAssembly/Class1.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace FrontendAssembly
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+    };
+}
