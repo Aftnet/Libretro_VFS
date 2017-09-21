1123bd7a1b6215b3bc6fce20a55f4f4cbb1d94db
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-09-21 16:20:17 +0900

    Submodule

--- FrontendAssembly/Class1.cpp
+++ FrontendAssembly/Class1.cpp
@@ -5,12 +5,12 @@ using namespace FrontendAssembly;
 using namespace Platform;
 using namespace Windows::Storage;
 
+EXTERN_C_START
+
 struct retro_vfs_file_handle
 {
 	StorageFile^ file;
 };
-
-EXTERN_C_START
 
 retro_vfs_file_handle *frontend_filestream_open(const char *path, uint64_t flags)
 {
